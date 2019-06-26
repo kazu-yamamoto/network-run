544a7fd4765e31237fd36e358faf0467d5771d15
Kazu Yamamoto <kazu@iij.ad.jp>
2019-06-26 09:45:44 +0900

    initial import.

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for network-run
+
+## 0.0.0
+
+* First version.
---
+++ Network/Run/TCP.hs
@@ -0,0 +1,48 @@
+
+-- | Simple functions to run TCP clients and servers.
+module Network.Run.TCP (
+    runTCPClient
+  , runTCPServer
+  ) where
+
+import Control.Concurrent (forkFinally)
+import qualified Control.Exception as E
+import Control.Monad (forever, void)
+import Network.Socket
+
+-- | Running a TCP client with a connected socket.
+runTCPClient :: HostName -> ServiceName -> (Socket -> IO a) -> IO a
+runTCPClient host port client = withSocketsDo $ do
+    addr <- resolve
+    E.bracket (open addr) close client
+  where
+    resolve = do
+        let hints = defaultHints { addrSocketType = Stream }
+        head <$> getAddrInfo (Just hints) (Just host) (Just port)
+    open addr = do
+        sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+        connect sock $ addrAddress addr
+        return sock
+
+-- | Running a TCP server with an accepted socket and its peer name.
+runTCPServer :: String -> (Socket -> SockAddr -> IO aa) -> IO a
+runTCPServer port server = withSocketsDo $ do
+    addr <- resolve
+    E.bracket (open addr) close loop
+  where
+    resolve = do
+        let hints = defaultHints {
+                addrFlags = [AI_PASSIVE]
+              , addrSocketType = Stream
+              }
+        head <$> getAddrInfo (Just hints) Nothing (Just port)
+    open addr = do
+        sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+        setSocketOption sock ReuseAddr 1
+        withFdSocket sock $ setCloseOnExecIfNeeded
+        bind sock (addrAddress addr)
+        listen sock 10
+        return sock
+    loop sock = forever $ do
+        (conn, peer) <- accept sock
+        void $ forkFinally (server conn peer) (\_ -> close conn)
