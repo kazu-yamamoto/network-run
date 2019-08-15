efdb969e6dc05fbcaeb777a691aace5c542b8887
Kazu Yamamoto <kazu@iij.ad.jp>
2019-08-15 10:09:52 +0900

    adding UDP examples.

---
+++ examples/udpClient.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+-- Echo client program
+module Main (main) where
+
+import qualified Data.ByteString.Char8 as C
+import Network.Run.UDP (runUDPClient)
+import Network.Socket
+import Network.Socket.ByteString (recvFrom, sendTo)
+
+main :: IO ()
+main = runUDPClient "127.0.0.1" "3000" $ \sock sockAddr -> do
+    -- Initially the local port is 0
+    my1 <- getSocketName sock
+    putStrLn $ "My sock addr " ++ show my1
+    putStrLn $ "Peer sock addr " ++ show sockAddr
+    _ <- sendTo sock "Hello, world!" sockAddr
+    -- After sendTo, the local port is implicitly bound
+    my2 <- getSocketName sock
+    putStrLn $ "My sock addr " ++ show my2
+    (msg, peer) <- recvFrom sock 1024
+    putStrLn $ "Peer sock addr " ++ show peer
+    putStr "Received: "
+    C.putStrLn msg
---
+++ examples/udpServer.hs
@@ -0,0 +1,11 @@
+module Main (main) where
+
+import Control.Monad (unless, forever, void)
+import qualified Data.ByteString as S
+import Network.Run.UDP (runUDPServer)
+import Network.Socket.ByteString (recvFrom, sendTo)
+
+main :: IO ()
+main = runUDPServer (Just "127.0.0.1") "3000" $ \sock -> forever $ do
+    (msg, peer) <- recvFrom sock 2048
+    unless (S.null msg) $ void $ sendTo sock msg peer
