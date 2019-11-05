ce09eda83fd1fccc145d4aa938418583915f1986
Kazu Yamamoto <kazu@iij.ad.jp>
2019-11-05 16:32:45 +0900

    implementing runUDPServerFork.

--- Network/Run/UDP.hs
+++ Network/Run/UDP.hs
@@ -2,10 +2,15 @@
 module Network.Run.UDP (
     runUDPClient
   , runUDPServer
+  , runUDPServerFork
   ) where
 
+import Control.Concurrent (forkFinally)
 import qualified Control.Exception as E
+import Control.Monad (forever, void)
+import Data.ByteString (ByteString)
 import Network.Socket
+import Network.Socket.ByteString
 
 import Network.Run.Core
 
@@ -19,8 +24,21 @@ runUDPClient host port client = withSocketsDo $ do
     let sockAddr = addrAddress addr
     E.bracket (openSocket addr) close $ \sock -> client sock sockAddr
 
--- | Running a UDP server with an open socket.
+-- | Running a UDP server with an open socket in a single Haskell thread.
 runUDPServer :: Maybe HostName -> ServiceName -> (Socket -> IO a) -> IO a
 runUDPServer mhost port server = withSocketsDo $ do
     addr <- resolve Datagram mhost port True
     E.bracket (openServerSocket addr) close server
+
+-- | Running a UDP server with a connected socket in each Haskell thread.
+--   The first request is given to the server.
+runUDPServerFork :: Maybe HostName -> ServiceName -> (Socket -> ByteString -> IO a) -> IO a
+runUDPServerFork mhost port server = runUDPServer mhost port $ \lsock -> forever $ do
+    (bs0,peeraddr) <- recvFrom lsock 1024
+    let family = case peeraddr of
+          SockAddrInet{}  -> AF_INET
+          SockAddrInet6{} -> AF_INET6
+          _                 -> error "family"
+    s <- socket family Datagram defaultProtocol
+    connect s peeraddr
+    void $ forkFinally (server s bs0) (\_ -> close s)
